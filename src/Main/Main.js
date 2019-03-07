dbf98efaccbeba4a6d130638ce3d5d586e4574ce
Victoria Godfrey <victorialgodfrey@gmail.com>
2019-03-07 15:01:53 -0500

    removed notes

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Main.css'
 import DeleteNote from '../DeleteNote/DeleteNote'
-
+import { Route } from 'react-router-dom';
 
 class Main extends React.Component {
 	render() {
@@ -9,7 +9,6 @@ class Main extends React.Component {
 			<div className="note">
 				<h3> {notes.name} </h3>
 				<p>Date Modified: {notes.modified}</p>
-				
 				{DeleteNote}
 			</div>)
 		return(
