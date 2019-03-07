cb83b0f371b4540af898dffec5bb1c73c74334ce
Victoria Godfrey <victorialgodfrey@gmail.com>
2019-03-07 13:40:38 -0500

    mild router changes

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Main.css'
+import DeleteNote from '../DeleteNote/DeleteNote'
 
 
 class Main extends React.Component {
@@ -8,6 +9,8 @@ class Main extends React.Component {
 			<div className="note">
 				<h3> {notes.name} </h3>
 				<p>Date Modified: {notes.modified}</p>
+				
+				{DeleteNote}
 			</div>)
 		return(
 			<main>
--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Sidebar.css'
 import FolderSidebar from '../FolderSidebar/FolderSidebar'
 import AddFolder from '../AddFolder/AddFolder'
-
+import { NavLink } from 'react-router-dom';
 
 class Sidebar extends React.Component {
 	render() {
