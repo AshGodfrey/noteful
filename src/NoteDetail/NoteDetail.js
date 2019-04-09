0e0dcd3c848cd658abf784f83e323749d4ff1178
Victoria Godfrey <victorialgodfrey@gmail.com>
2019-04-09 16:25:31 -0400

    added basic delete note functionality

--- src/NoteDetail/NoteDetail.js
+++ src/NoteDetail/NoteDetail.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import DeleteNote from '../DeleteNote/DeleteNote'
 import './NoteDetail.css'
+import { Link } from 'react-router-dom';
 import ApiContext from '../ApiContext';
 import config from '../Config';
 
 class NoteDetail extends React.Component {
-	
+	static defaultProps ={
+	  onDeleteNote: () => {},
+	  }
+	  static contextType = ApiContext;
+
+	  handleClickDelete = e => {
+	   
+	    const noteId = this.props.activeNote
+
+	    fetch(`${config.API_ENDPOINT}/notes/${noteId.activeNote.id}`, {
+	      method: 'DELETE',
+	      headers: {
+	        'content-type': 'application/json'
+	      },
+	    })
+	      .then(res => {
+	        if (!res.ok)
+	          return res.json().then(e => Promise.reject(e))
+	          return res.json()
+	      })
+	      .then(() => {
+	        this.context.deleteNote(noteId)
+	        this.props.onDeleteNote(noteId)
+	      })
+	  }
+
 	render() {
 		
 		if (this.props) {
@@ -18,7 +43,7 @@ class NoteDetail extends React.Component {
 				<div className="noteinfo">
 					<h1>{activeNote.name}</h1>
 					<p>{activeNote.modified}</p>
-					<DeleteNote onClick={this.handleClickDelete} />
+					<Link to="/"><button onClick={this.handleClickDelete}>Delete Note</button></Link>
 				</div>
 				<p>{activeNote.content}</p>
 			</main>
