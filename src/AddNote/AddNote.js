2845a43a01bef4be3e951e41d7119ac615f9bedf
Victoria Godfrey <victorialgodfrey@gmail.com>
2019-04-18 17:17:49 -0400

    add requirements

--- src/AddNote/AddNote.js
+++ src/AddNote/AddNote.js
@@ -14,31 +14,33 @@ export default class AddNote extends Component {
 
   handleSubmit = e => {
     e.preventDefault()
-    const newNote = {
-      name: e.target['note-name'].value,
-      content: e.target['note-content'].value,
-      folderId: e.target['note-folder-id'].value,
-      modified: new Date(),
-    }
-    fetch(`${Config.API_ENDPOINT}/notes`, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(newNote),
-    })
-      .then(res => {
-        if (!res.ok)
-          return res.json().then(e => Promise.reject(e))
-        return res.json()
+   
+      const newNote = {
+        name: e.target['note-name'].value,
+        content: e.target['note-content'].value,
+        folderId: e.target['note-folder-id'].value,
+        modified: new Date(),
+      }
+      fetch(`${Config.API_ENDPOINT}/notes`, {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(newNote),
       })
-      .then(note => {
-        this.context.addNote(note)
-        this.props.history.push(`/folder/${note.folderId}`)
-      })
-      .catch(error => {
-        console.error({ error })
-      })
+        .then(res => {
+          if (!res.ok)
+            return res.json().then(e => Promise.reject(e))
+          return res.json()
+        })
+        .then(note => {
+          this.context.addNote(note)
+          this.props.history.push(`/folder/${note.folderId}`)
+        })
+        .catch(error => {
+          console.error({ error })
+        })
+      
   }
 
   render() {
@@ -51,19 +53,19 @@ export default class AddNote extends Component {
             <label htmlFor='note-name-input'>
               Name
             </label>
-            <input type='text' id='note-name-input' name='note-name' />
+            <input type='text' id='note-name-input' name='note-name' required/>
           </div>
           <div className='field'>
             <label htmlFor='note-content-input'>
               Content
             </label>
-            <textarea id='note-content-input' name='note-content' />
+            <textarea id='note-content-input' name='note-content' required />
           </div>
           <div className='field'>
             <label htmlFor='note-folder-select'>
               Folder
             </label>
-            <select id='note-folder-select' name='note-folder-id'>
+            <select id='note-folder-select' name='note-folder-id' required>
               <option value={null}>...</option>
               {folders.map(folder =>
                 <option key={folder.id} value={folder.id}>
